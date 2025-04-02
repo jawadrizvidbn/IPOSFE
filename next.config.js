02240499bb78717290aaa7e1bc1756d7b29fccc9
adil2190 <madil219076@gmail.com>
2025-04-03 00:07:12 +0500

    disabled esline

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
-"use strict";
+'use strict'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: process.env.BASEPATH,
-    redirects: async () => {
-        return [
-            {
-                source: '/',
-                destination: '/en',
-                permanent: true,
-                locale: false
-            }
-        ];
-    },
-    // TODO: below line is added to resolve twice event dispatch in the calendar reducer
-    reactStrictMode: false
-};
-module.exports = nextConfig;
+  eslint: {
+    ignoreDuringBuilds: true
+  },
+  basePath: process.env.BASEPATH,
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/en',
+        permanent: true,
+        locale: false
+      }
+    ]
+  },
+  // TODO: below line is added to resolve twice event dispatch in the calendar reducer
+  reactStrictMode: false
+}
+module.exports = nextConfig
